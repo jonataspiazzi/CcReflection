2de65a179f3191fb375493e29a8b55ef3543600c
Jonatas Piazzi <piazzi_7@hotmail.com>
2018-01-26 16:39:09 -0200

    First commit.

---
+++ src/CcReflection/CcReflection/TypeExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CcReflection
+{
+    public static class TypeExtension
+    {
+        public static bool IsExtendedPrimitive(this Type type)
+        {
+            type = Nullable.GetUnderlyingType(type) ?? type;
+
+            if (type.IsPrimitive) return true;
+            if (type.IsEnum) return true;
+            if (type == typeof(decimal)) return true;
+            if (type == typeof(string)) return true;
+            if (type == typeof(DateTime)) return true;
+            if (type == typeof(TimeSpan)) return true;
+
+            return false;
+        }
+    }
+}
